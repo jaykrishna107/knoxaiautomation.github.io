ed0dc83bee2849906871ca756c98ffbf58988879
jaykrishna107 <bongujayakrishna@gmail.com>
2026-01-25 13:37:59 +0530

    Fix GitHub Pages user site setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/knoxaiautomation.github.io/",
   plugins: [react()],
   resolve: {
     alias: {
